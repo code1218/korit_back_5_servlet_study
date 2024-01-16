074caeadfbf4f4c3f2f0c391bdd14e484da6eeac
code1218 <skjil1218@gmail.com>
2024-01-16 17:12:39 +0900

    init: project first commit

---
+++ src/main/java/com/study/servlet_study/servlet/HttpStudyServlet.java
@@ -0,0 +1,69 @@
+package com.study.servlet_study.servlet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.study.servlet_study.utils.ParamsConverter;
+
+@WebServlet("/http")
+public class HttpStudyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HttpStudyServlet() {
+        super();
+    }
+
+    // HTTP 메소드
+    // POST요청 	-> C reate(추가)
+    // GET요청 		-> R ead(조회)
+    // PUT요청 		-> U pdate(수정)
+    // DELETE요청 	-> D elete(삭제)
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map<String, String> paramsMap = new HashMap<>();
+		
+		request.getParameterMap().forEach((k, v) -> {
+			StringBuilder builder = new StringBuilder();
+			
+			Arrays.asList(v).forEach(value -> builder.append(value));
+			
+			paramsMap.put(k, builder.toString());
+		});
+		
+		System.out.println(paramsMap);
+		
+		Map<String, String> paramsMap2 = new HashMap<>();
+		Iterator<String> ir = request.getParameterNames().asIterator();
+		while(ir.hasNext()) {
+			String key = ir.next();
+			paramsMap2.put(key, request.getParameter(key));
+		}
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Map<String, String> paramsMap = ParamsConverter.convertParamsMapToMap(request.getParameterMap());
+				
+		System.out.println(paramsMap);
+	}
+	
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
